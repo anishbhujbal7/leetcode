4cefaa38dc0d743f1e5ef594c224b71bf8598aca
anishbhujbal7 <anishbhujbal77@gmail.com>
2025-04-07 18:28:16 +0530

    121.cpp
    You are given an array prices where prices[i] is the price of a given stock on the ith day.
    
    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
    
    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.

--- 121.cpp
+++ 121.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice=INT_MAX;
-        int maxProfit=0;
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<=minPrice){
-                minPrice=prices[i];
-
-            }
-            else if (prices[i]-minPrice>maxProfit){
-                maxProfit=prices[i]-minPrice;
-
-            }
+        int minPrice=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.size();i++){
+           minPrice=min(minPrice,prices[i]);
+           int currProfit=prices[i]-minPrice;
+           profit=max(profit,currProfit);
         }
-        return maxProfit;
+        return profit;
     }
 };
