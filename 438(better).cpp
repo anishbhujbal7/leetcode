dd51275541d07d67165ea495bd76b913b3392edb
anishbhujbal7 <anishbhujbal77@gmail.com>
2025-06-25 17:17:09 +0530

    Update 438(better).cpp
    Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.

--- 438(better).cpp
+++ 438(better).cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> hashMap;
-        int n=nums.size();
-        vector<int> res;
-        for(int i=0;i<n;i++){
-            hashMap[nums[i]]++;
+    vector<int> findAnagrams(string s, string p) {
+       int m=s.size();
+       int n=p.size();
+       
+       vector<int> res;
+       vector<int> hash(26,0);
+        for(int j=0;j<n;j++){
+            hash[p[j]-'a']++;
         }
-        vector<int> sortedVec;
-        for(auto i:hashMap){
-            sortedVec.push_back(i.second);
-        }
-        sort(sortedVec.begin(),sortedVec.end(), greater<int>());
-        for(int i=0;i<sortedVec.size();i++){
-            if(k==0) break;
-            int target=sortedVec[i];
-            for(auto pair:hashMap){
-                if(pair.second==target){
-                    res.push_back(pair.first);
-                    hashMap[pair.first]=-1;
-                    k--;
-                }
+        
+        for(int i=0;i<m;i++){
+            vector<int> sCount(26,0);
+            string str=s.substr(i,n);
+            for(char ch:str){
+                sCount[ch-'a']++;
             }
+            if(hash==sCount) res.push_back(i);
         }
-        return res;
+       return res; 
     }
 };
