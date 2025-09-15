cbb1f2bc2ab858e1c0f6bb1965bbd2cc8d5884ae
anishbhujbal7 <anishbhujbal77@gmail.com>
2025-09-16 01:00:49 +0530

    Create 2351(string).cpp
    Given a string s consisting of lowercase English letters, return the first letter to appear twice.
    
    Note:
    
    A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
    s will contain at least one letter that appears twice.

---
+++ 2351(string).cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int> map;
+        for(char c: s){
+            map[c]++;
+            if(map[c]==2) return c;
+        }
+        return 'a';
+    }
+};
