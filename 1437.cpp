e47b1147d77b354d4b8e1d56a4b3c34699a2c1a3
anishbhujbal7 <anishbhujbal77@gmail.com>
2025-11-17 23:31:42 +0530

    Create 1437.cpp
    Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.

---
+++ 1437.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int count =k;
+        for(auto num : nums){
+            if(num == 1){
+                if(count<k) return false;
+                else count = 0;
+            }else{
+                count++;
+            }
+        }
+        return true;
+    }
+};
