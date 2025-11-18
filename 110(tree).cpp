d72b17c0d453efdda5a360aeedc30210306faeda
anishbhujbal7 <anishbhujbal77@gmail.com>
2025-11-18 15:00:42 +0530

    Create 110(tree).cpp
    Given a binary tree, determine if it is height-balanced.

---
+++ 110(tree).cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+       return checkBalance(root) == -1 ? false : true; 
+    }
+    int checkBalance(TreeNode* node){
+        if(node == nullptr) return  0;
+        int lh = checkBalance(node->left);
+        if(lh == -1) return -1;
+        int rh = checkBalance(node->right);
+        if(rh == -1) return -1;
+        if(abs(lh - rh)>1) return -1;
+        return max(lh,rh)+1;
+    }
+};
